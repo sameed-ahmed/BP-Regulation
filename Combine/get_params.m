2ded86e10c4914e1033c3bccafbefd76ce3b2ac1
Work <work@Jessicas-MacBook-Pro.local>
2019-11-07 11:52:43 -0500

    Files for human model ready to combine with rat model

---
+++ Combine/get_params.m
@@ -0,0 +1,148 @@
+function pars = get_params(species,sex,AA)
+if     strcmp(sex, 'male')
+    gen = 1;
+elseif strcmp(sex, 'female')
+    gen = 0;
+end
+%disp([species{human+1},' ',gender{gg},' ',num2str(AA)])
+
+% Scaling factor
+% Rat flow = Human flow x SF
+if strcmp(species, 'human') 
+    SF = 1.0;
+    human = 1;
+elseif strcmp(species, 'rat') 
+    SF = 4.5*10^(-3);
+    human = 0;
+end
+
+N_rsna      = 1*AA;
+R_aass      = 31.67 / SF;   % mmHg min / l
+R_eass      = 51.66 / SF;   % mmHg min / l
+P_B         = 18;           % mmHg
+P_go        = 28;           % mmHg
+C_gcf       = 0.00781 * SF;
+eta_etapt   = 0.8; 
+eta_epsdt   = 0.5; 
+eta_etacd   = 0.93; 
+K_vd        = 0.00001;
+K_bar       = 16.6 / SF;    % mmHg min / l
+R_bv        = 3.4 / SF;     % mmHg min / l
+T_adh       = 6;            % min
+Phi_sodin   = 0.126 * SF;   % mEq / min
+%if     strcmp(gender{gg}, 'male')
+%   C_K      = 6;            % mEq / l 
+%elseif strcmp(gender{gg}, 'female')
+   C_K      = 5;            % mEq / l 
+%end
+T_al        = 30;           % min LISTED AS 30 IN TABLE %listed as 60 in text will only change dN_al
+N_rs        = 1;            % ng / ml / min
+
+% RAS
+h_renin     = 12;      % min
+h_AGT       = 10*60;   % min
+h_AngI      = 0.5;     % min
+h_AngII     = 0.66;    % min
+h_Ang17     = 30;      % min
+h_AngIV     = 0.5;     % min
+h_AT1R      = 12;      % min
+h_AT2R      = 12;      % min
+
+c_GPautoreg = 5;
+P_ghnom     = 62;      % mmHg
+
+if strcmp(species, 'human') 
+    X_PRCPRA = 61/60.0; %fmol/min/pg
+    if  strcmp(sex, 'male')     
+        k_AGT   = 577.04;
+        c_ACE   = 0.88492;
+        c_Chym  = 0.09315;
+        c_NEP   = 0.038189;
+        c_ACE2  = 0.0078009;
+        c_IIIV  = 0.25056;
+        c_AT1R  = 0.17008;
+        c_AT2R  = 0.065667;
+        AT1R_eq = 13.99;
+        AT2R_eq = 5.0854;
+        ALD_eq = 85;
+    elseif strcmp(sex, 'female')
+        k_AGT   = 610.39;
+        c_ACE   = 1.4079;
+        c_Chym  = 0.1482;
+        c_NEP   = 0.060759;
+        c_ACE2  = 0.0037603;
+        c_IIIV  = 0.038644;
+        c_AT1R  = 0.027089;
+        c_AT2R  = 0.038699;
+        AT1R_eq = 3.78;
+        AT2R_eq = 5.0854;
+        ALD_eq = 69.1775;
+
+    end
+    pt_sod_reab_EQ = 13.909;%15.285;%;14.4;%
+    dt_sod_reab_EQ = 1.5859;%2.0714;%1.8;
+    cd_sod_reab_EQ = 1.6909;%1.6934;%1.674;
+
+ 
+elseif strcmp(species, 'rat') 
+    % Male and female different parameters for RAS
+    if     strcmp(sex, 'male')
+        C_K      = 6;
+        X_PRCPRA = 135.59/17.312;
+        k_AGT   = 801.02;
+        c_ACE   = 0.096833;
+        c_Chym  = 0.010833;
+        c_NEP   = 0.012667;
+        c_ACE2  = 0.0026667;
+        c_IIIV  = 0.29800;
+        c_AT1R  = 0.19700;
+        c_AT2R  = 0.065667;
+        AT1R_eq = 20.46;
+        AT2R_eq = 6.82;
+        ALD_eq = 395.3;
+
+    elseif strcmp(sex, 'female')
+        X_PRCPRA = 114.22/17.312;
+        k_AGT   = 779.63;
+        c_ACE   = 0.11600;
+        c_Chym  = 0.012833;
+        c_NEP   = 0.0076667;
+        c_ACE2  = 0.00043333;
+        c_IIIV  = 0.29800;
+        c_AT1R  = 0.19700;
+        c_AT2R  = 0.065667;
+        AT1R_eq = 20.46;
+        AT2R_eq = 6.82;
+        ALD_eq = 379.4;
+
+    end
+    pt_sod_reab_EQ = 0.068294039337572;
+    dt_sod_reab_EQ = 0.008094477703862;
+    cd_sod_reab_EQ = 0.007616239742696;
+end
+amount = 0.08;
+slope = 8.5;
+%baseline = 0.02;
+if     strcmp(sex, 'male')
+    if     AA>1
+        baseline = 0.0182;
+    else
+        baseline = 0.0193;
+    end
+else
+    if     AA>1
+        baseline = 0.0181;
+    else
+        baseline = 0.0199;
+    end
+end
+
+
+pars = [N_rsna; R_aass; R_eass; P_B; P_go; C_gcf; eta_etapt; eta_epsdt; ...
+        eta_etacd; K_vd; K_bar; R_bv; T_adh; Phi_sodin; C_K; T_al; ...
+        N_rs; X_PRCPRA; h_renin; h_AGT; h_AngI; h_AngII; h_Ang17; ...
+        h_AngIV; h_AT1R; h_AT2R; c_GPautoreg; P_ghnom; k_AGT; c_ACE; ...
+        c_Chym; c_NEP; c_ACE2; c_IIIV; c_AT1R; c_AT2R; AT1R_eq; ...
+        AT2R_eq; ALD_eq; gen; ...
+        pt_sod_reab_EQ; dt_sod_reab_EQ; cd_sod_reab_EQ;amount;slope; baseline; human; SF];
+end
---
+++ Combine/run_sim_treatments.m
@@ -0,0 +1,145 @@
+% This simulates the blood pressure regulation model blood_press_reg.m.
+% 
+% Parameters are given by:
+% "Long-Term Mathematical Model Involving Renal Sympathetic Nerve Activity,
+% Arterial Pressure, and Sodium Excretion" - 2005 - Karaaslan, et. al.
+% "Sex-specific Long-term Blood Pressure Regulation: Modeling and Analysis"
+% - 2018 - Leete, Layton.
+% 
+% Steady state data is calculated by solve_ss_numerical.m.
+
+function run_sim_treatments(human,gg,IC, varargin)
+species = {'rat','human'};
+gender     = {'male', 'female'};
+
+%% Define default imputs
+AA = 1;
+ACEI = 0;
+furosemide = 0;
+NSAID = 0;
+myo_ind = 0;
+water_ind = 0;
+
+%% Read and assign optional variables
+for i = 1:2:length(varargin)
+    if strcmp(varargin{i},'ACEI')
+        ACEI = varargin{i + 1}; %ACEI indicator
+    elseif strcmp(varargin{i},'furosemide')
+        furosemide = varargin{i + 1}; %furosemide indicator
+    elseif strcmp(varargin{i},'NSAID')
+        NSAID = varargin{i+1}; %indicator
+   elseif strcmp(varargin{i},'Myogenic Response')
+        myo_ind = varargin{i+1}; %indicator 0 for normal, 1 for impaired
+   elseif strcmp(varargin{i},'Water Intake')
+        water_ind = varargin{i+1};%indicator 0 for normal, 1 for low
+    elseif strcmp(varargin{i},'RSNA')
+        AA = varargin{i+1}; %multiply N_rsna by to simulate hypertension
+    end
+end
+
+pars = get_params(species{human+1},gender{gg},AA);
+
+
+kappa_ACEI = 0;
+kappa_f = 0;
+kappa_f_md = 0;
+
+   if ACEI == 1
+       kappa_ACEI = 0.76;
+   elseif ACEI > 1
+       kappa_ACEI = 0.90;
+   end
+   if furosemide == 1
+       kappa_f = 0.15;
+       kappa_f_md = 0.4;
+   elseif furosemide == 2
+       kappa_f = 0.3;
+       kappa_f_md = 0.5;
+   end  
+
+%% Solve DAE
+
+% Initial value
+% This initial condition is the steady state data value taken from
+% experiments (CITE). Therefore, the initial condition of the derivative is
+% 0.
+
+
+% Load data for steady state initial value. 
+load(IC,'SSdata');
+SS_data_IC = SSdata;
+
+% Initial condition for the variables and their derivatives. 
+% System is initially at steady state, so the derivative is 0.
+x0 = SS_data_IC; x_p0 = zeros(84,1);
+
+% Time at which to keep steady state, change a parameter, etc.
+tchange = 100;
+
+% Initial time (min); Final time (min);\
+num_days =2;
+t0 = 0*1440; tf = tchange + num_days*1440;
+% Time vector; %if any simulation fails, try adjusting this
+%tspan = [t0, tf];
+tspan = t0:10:tf;
+% Solve dae
+[t,x] = ode15i(@(t,x,x_p) bp_reg_sim(t,x,x_p,pars,tchange,...
+                                     'ACEI',kappa_ACEI,'furosemide',[kappa_f,kappa_f_md],'NSAID',NSAID',...
+                                     'Myogenic Response',myo_ind,'Water Intake',water_ind), ...
+                tspan, x0, x_p0);
+
+if not (isreal(x))
+    disp('Imaginary number returned.')
+end
+
+% %Save Data
+save_name_ending = '';
+if water_ind
+    save_name_ending = strcat(save_name_ending,'_lowwaterintake');
+end
+if myo_ind
+    save_name_ending = strcat(save_name_ending,'_impairedmyo');
+end
+
+
+filename = '%s%s_%s_%s_%s_%s_%s%s.mat';
+
+
+filename = sprintf( filename,'Data/',species{human+1},gender{gg},num2str(ACEI),num2str(furosemide),num2str(NSAID),num2str(AA),save_name_ending);
+%filename
+save(filename, 't','x');
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
